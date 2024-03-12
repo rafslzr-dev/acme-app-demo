4be218fd47278cfd9ea25216b2df842d8af5023d
rafslzr-dev <rafslzr.dev@gmail.com>
2024-03-12 22:18:17 +0800

    Added input state error

--- app/Dashboard/Dashboard.tsx
+++ app/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import styles from './Dashboard.module.scss'
 import { Button, Space, Popover } from 'antd';
 import { useForm, SubmitHandler } from "react-hook-form"
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 
 
 type InputsTypes = {
@@ -28,15 +29,21 @@ export const Dashboard: React.FC = () => {
       <h2 className={styles.greetings}>Hello! How many dogs did you walk today?</h2>
       <Space.Compact style={{ maxWidth: '520px', width: '100%' }}>
         <Popover
-          title="Only accepts 1-15"
+          title="Only accepts 1-10. Field is required"
           open={!!errors.dogCount}
-          placement='bottom'
+          placement='topLeft'
+          trigger='focus'
         >
-          <input id="dog-count" type="number" className='input -left-radius' min={1} max={15} {...register('dogCount', { min: 1, max: 15 })}/>
+          <input id="dog-count"
+            type="number"
+            className={classNames('input -left-radius', !!errors.dogCount && '-error' )}
+            min={1}
+            max={15}
+            {...register('dogCount', { min: 1, max: 10, required: true })}/>
         </Popover>
         <Button type='primary' size='large' onClick={handleSubmit(onSubmit)}>Submit</Button>
       </Space.Compact>
-      <p className='input-label'>Max: 15</p>
+      <p className='input-label'>Max: 15. Required.</p>
     </AppLayout>
   )
 
--- app/WalkLog/WalkLog.tsx
+++ app/WalkLog/WalkLog.tsx
@@ -3,7 +3,7 @@ import styles from './WalkLog.module.scss'
 
 export const WalkLog: React.FC = () => (
     <AppLayout>
-      <h2 className={styles.greetings}>Walk Log</h2>
+      <h2 className={styles.greetings}>Log your dog walks here:</h2>
     </AppLayout>
   )
 
